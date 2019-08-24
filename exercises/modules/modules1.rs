1888426e8dccea385561d8e3f5a6af47126af629
Jose Caban <asskoala@gmail.com>
2019-08-24 13:28:09 -0400

    macros and modules

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,11 +1,11 @@
 // modules1.rs
 // Make me compile! Scroll down for hints :)
 
-mod sausage_factory {
-    fn make_sausage() {
+pub mod sausage_factory {
+    pub fn make_sausage() {
         println!("sausage!");
     }
-}
+} 
 
 fn main() {
     sausage_factory::make_sausage();
